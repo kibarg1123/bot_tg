8e77d43cdef94471d829fc91257cba2a2c25812e
dmikaliuk@mail.ru <magnat11234>
2022-05-13 23:12:38 +0300

    copy site news

---
+++ main.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeatifulSoup
+from datetime import datetime
+import time
+
+def get_first_news():
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36"
+    }
+    url = "https://www.securitylab.ru/news/"
+    r = requests.get(url=url, headers=headers)
+    soup = BeatifulSoup(r.text, "lxml")
+    articles_cards = soup.find_all("a", class_="article-card")
+
+    for articles in articles_cards:
+        article_title = article.find("h2", class_="article-card-title").text.strip()
+        article_desc = article.find("p").text.strip()
+        article_url = f'https://www.securitylab.ru{article_get("href")}'
+
+        article_date_time = article.find("time").get("datatime")
+        date_from_iso = datetime.fromisoformat(article_date_time)
+        date_time = datetime.strftime(date_from_iso, "%Y-%m-%d %H:%M:%S")
+        article_date_timestamp = time.mktime(datetime.strftime(date_time, "%Y-%m-%d %H:%M:%S").tuple())
+
+
+        print(f"{article_title} | {article_url} | {article_date_timestamp}")
+
+
+    get_first_news()
